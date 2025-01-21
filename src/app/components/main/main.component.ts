2a498dd10fd15db402b9259a1e7b5655a4fdd411
evelyne802 <linagluzman@gmail.com>
2025-01-21 19:30:44 +0200

    feat: contact info buttons + css for mobile view

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -9,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
 
+  siteInEnglish = true;
+  languageIcon = '../../assets/images/us-flag.png';
+  titleDescription: string = 'שף מומחה לאוכל יפני';
+  descriptionClasses: string = 'subtitle hebrew-font';
+
+  ngOnInit(){
+    this.languageChange();
+  }
+
+  languageChange(){
+    this.siteInEnglish = !this.siteInEnglish;
+    if(this.siteInEnglish){
+      this.englishTranslate();
+      this.languageIcon = '../../assets/images/israel-flag.png';
+    } else {
+      this.hebrewTranslate();
+      this.languageIcon = '../../assets/images/us-flag.png';
+    }
+  }
+
+  hebrewTranslate() {
+    this.titleDescription = 'שף מומחה לאוכל יפני';
+    this.descriptionClasses = 'subtitle hebrew-font';
+  }
+
+  englishTranslate() {
+    this.titleDescription = 'Japanese Private Chef';
+    this.descriptionClasses = 'subtitle';
+  }
+
 }
